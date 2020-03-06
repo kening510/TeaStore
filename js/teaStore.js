d5858db51145ba0e4462a49acd40131ff8e8a082
Kening Fohlin <kening.fohlin@gmail.com>
2020-03-06 19:11:11 +0100

    reading teas from json

---
+++ js/teaStore.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+   loadJSON();
+})
+
+function loadJSON() {
+    console.log("Loading JSON");
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET','../json/teaStore.json',true);
+    xhr.send();
+    xhr.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200 ){
+            console.log(this);   // there is no resonpseJSON
+
+            let data = this.responseText;
+            //console.log(json);
+
+            
+            let json = JSON.parse(data);
+            console.log(json);
+            
+            
+            json.teas.forEach(tea => {
+                $("#teaList").append('<li id="' + tea.id +'"> ' +  tea.name + '</li>')
+            });
+            
+
+            }
+        }
+}
+
