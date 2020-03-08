8876d83513d28bc8b3fc6b0a1ddc738e0e1f301e
Kening Fohlin <kening.fohlin@gmail.com>
2020-03-08 22:26:52 +0100

    showing all teas in the list

--- js/teaStore.js
+++ js/teaStore.js
@@ -21,7 +21,36 @@ function loadJSON() {
 
 
             json.teas.forEach(tea => {
-                $("#tea-list").append('<li class ="list-group-item" id="' + tea.id + '"> ' + tea.name + '</li>')
+                $("#tea-list").append(`<li class="list-group-item">
+                 <div class="row">
+                    <div class="col-3"><img src="` + tea.image + `" class="img-fluid" alt="Black tea">
+                    </div>
+                    <div class="col-9">
+                        <div class="row">
+                            <div class="col-12">
+                                <h4>`+ tea.name + `</h4>
+                                <p> This tea comes from ` + tea.country + ` | ` + tea.steepingTime + ` <br> Best before ` + tea.bestBefore +`</p> 
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2">
+                                <div class="price">
+                                    <p> ` + tea.price + `</p>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <button type="button" class="btn btn-success">+</button>
+                                <input type="text" size="1" name="number">
+                                <button type="button" class="btn btn-success">-</button>
+                            </div>
+                            <div class="col-4">
+                                <button type="button" class="btn btn-success btn-sm">add to cart</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </li>`)
             });
 
 
